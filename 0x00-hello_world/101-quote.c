09e5e22a048fb1987386e6ebdcfabff788086409
degiat <degiatb.k@gmail.com>
2022-06-30 11:02:04 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,22 +1,15 @@
 #include <stdio.h>
 /**
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * main - Entry point   
+ * Description
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
 {
-
-		fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-
-
-
-			return (1);
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
 
 }
