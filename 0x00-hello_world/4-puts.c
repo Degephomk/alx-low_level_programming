909f543dfd30a8ccc36508c18b1a8986e211cede
Degephomk <99641552+Degephomk@users.noreply.github.com>
2022-06-30 20:18:35 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-/*
+/**
  * main - Entry point
- * Return: description of expression returned
+ *Description:puts print
+ * Return: return value
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	return (0);
 }
