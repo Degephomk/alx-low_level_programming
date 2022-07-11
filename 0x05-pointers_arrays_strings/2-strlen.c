54cbc5c17bf49822afa308129706fe50ef642938
degiat <degiatb.k@gmail.com>
2022-07-11 05:50:22 -0700

    adding

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,13 +6,11 @@
  *
  * Return: The length of @str.
  */
-
-
-size_t  _strlen(char *s)
+size_t _strlen(const char *str)
 {
 	size_t length = 0;
 
-	while (*s++)
+	while (*str++)
 		length++;
 
 	return (length);
