8ce04c320969b67607e1e75868eb87469b1fee8f
degiat <degiatb.k@gmail.com>
2022-08-01 06:30:47 -0700

    0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,8 @@
-#ifndef _object_like_macro_h_
-#define _object_like_macro_h_
+#ifndef _0_OBJECT_LIKE_MACRO_H
+#define _0_OBJECT_LIKE_MACRO_H
 
 #define SIZE 1024
 
-#endif /* _object_like_macro_h_ */
+#endif
+
+
